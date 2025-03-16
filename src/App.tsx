d50797f333162cfb1b8ad1855c6b95037b2a7c5b
Ajayjayendran Arulraj <ajayjayendran.ar@gmail.com>
2025-03-16 19:25:20 +0530

    Remove unused codes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import EarningsCalendar from "./widgets/EarningsCalendar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <EarningsCalendar />
-    </>
-  );
+  return <EarningsCalendar />;
 }
 
 export default App;
